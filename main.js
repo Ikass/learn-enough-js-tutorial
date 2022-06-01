f20d6fbf4ec0a3df6e8e07e36faa422028f749db
Reinis Ikass <reinis.ikass@gmail.com>
2022-06-01 22:22:50 +0300

    update palindrome

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 let Phrase = require("ikass-palindrome");
 
+// initial test to see if everything works
 // alert(new Phrase("Madam, I'm Adam.").palindrome());
 
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
+function palindromeTester(event) {
+    event.preventDefault();
 
-if (phrase.palindrome()) {
-    alert(`"${phrase.content}" is a palindrome!`);
-} else {
-    alert(`"${phrase.content}" is not a palindrome.`);
+    let phrase = new Phrase(event.target.phrase.value);
+
+    let palindromeResult = document.querySelector("#palindromeResult");
+
+    if (phrase.palindrome()) {
+        palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong> is a palindrome!`;
+    } else {
+        palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong> is not a palindrome.`;
+    }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    let tester = document.querySelector("#palindromeTester");
+    tester.addEventListener("submit", function(event) {
+        palindromeTester(event);
+    });
+});
